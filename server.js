c5f490a0ce085b445c6205e748e9a044637b0ef9
Jason Young <jason@ytechie.com>
2015-12-02 15:48:36 -0600

    Added base64 support

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
 var querystring = require('querystring');
 
+
 http.createServer(function(req, res) {
 	var params = querystring.parse(req.url);
-	res.end(params['/?echo']);
+	if(params['/?echo']) {
+		res.end(params['/?echo']);
+	}
+	if(params['/?echo64']) {
+		var b = new Buffer(params['/?echo64'], 'base64');
+		res.end(b.toString());
+	}
 }).listen(process.env.PORT || 1337);
